1e5bcb5b694790ce3b2c5a9938e81d6e01d5784c
EkoueJojo <92011395+EkoueJojo@users.noreply.github.com>
2024-04-27 19:14:18 +0200

    Display guess results

--- scripts/game.js
+++ scripts/game.js
@@ -5,11 +5,11 @@ getUniverseData(universeFileName).then
 (
 	universeData =>
 	{
-		updatePage(universeData);
+		ShowPageContent(universeData);
 		retrieveCharacters(universeData);
 
 		let answer = getAnswerOfTheDay(universeData.characters, 0, true);
-		document.getElementById("GuessButton").addEventListener("click", function () { guess(universeData, answer); });
+		document.getElementById("GuessButton").addEventListener("click", function () { guess(universeData, answer[language]); });
 	}
 );
 
@@ -63,8 +63,12 @@ async function retrieveCharacters(universeData)
 function guess(universeData, answer)
 {
 	let characterField = document.getElementById("GuessField");
+	let guessesBody = document.getElementById("GuessesBody");
 	let attemptName = characterField.value;
 	let attempt = null;
+
+	if (attemptName == "")
+		return;
 
 	for (const character of universeData.characters)
 	{
@@ -74,26 +78,80 @@ function guess(universeData, answer)
 		}
 	}
 
-	console.log(universeData.metadata);
+	let attemptRow = document.createElement("tr");
+	let fieldNames = Object.keys(universeData.metadata.fieldNames[language]);
+
+	for (let i = 0; i < fieldNames.length; i++)
+	{
+		let attemptFieldValue;
+		let answerFieldValue;
+
+		if (fieldNames[i] == "names")
+		{
+			console.log(attempt.names[0], answer.names[0]);
+			attemptFieldValue = attempt.names[0];
+			answerFieldValue = answer.names[0];
+		}
+		else
+		{
+			console.log(attempt[fieldNames[i]], answer[fieldNames[i]]);
+			attemptFieldValue = attempt[fieldNames[i]];
+			answerFieldValue = answer[fieldNames[i]];
+		}
+
+		let attemptFieldElement = document.createElement("td");
+		attemptFieldElement.className = "AttemptField";
+
+		let attemptValues;
+
+		if (typeof attemptFieldValue != "object")
+		{
+			attemptValues = [attemptFieldValue];
+		}
+		else
+		{
+			attemptValues = attemptFieldValue;
+		}
+
+		if (attemptValues.length <= 0)
+		{
+			attemptValues = "×";
+		}
+
+		for (let value of attemptValues)
+		{
+			let valueParagraph = document.createElement("p");
+			valueParagraph.innerText = value;
+			attemptFieldElement.appendChild(valueParagraph);
+		}
+
+		if (attemptFieldValue == answerFieldValue)
+		{
+			attemptFieldElement.className += " Correct";
+		}
+
+		attemptRow.appendChild(attemptFieldElement);
+	}
+
+	guessesBody.prepend(attemptRow);
 
 	console.log(attempt);
 	console.log(answer[language]);
 }
 
-function updatePage(universeData)
+function ShowPageContent(universeData)
 {
+	let guessesHeadElement = document.getElementById("GuessesHead");
 	document.getElementById("Title").innerText = universeData.metadata.universeName;
 	document.getElementsByTagName("Title")[0].innerText = "Universdle - " + universeData.metadata.universeName;
 
-	for (const fieldName in universeData.metadata.fieldNames[language])
+	for (let fieldName in universeData.metadata.fieldNames[language])
 	{
 		if (Object.hasOwnProperty.call(universeData.metadata.fieldNames[language], fieldName))
 		{
-			if (fieldName != "hints")
-			{
-				let headerText = universeData.metadata.fieldNames[language][fieldName];
-				console.log(fieldName, headerText);
-			}
+			let headCell = document.createElement("th");
+			headCell.innerText = universeData.metadata.fieldNames[language][fieldName];
+			guessesHeadElement.appendChild(headCell);
 		}
 	}
 }
